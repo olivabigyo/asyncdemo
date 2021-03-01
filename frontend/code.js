59cc5934a336729026b61660ed98041bc01391b4
Kata <35190838+olivabigyo@users.noreply.github.com>
2021-03-01 16:15:16 +0100

    pdf slides

--- frontend/code.js
+++ frontend/code.js
@@ -1,25 +1,53 @@
 'use strict';
 
+// This is the client site javascript, which the user gets from the frontend server
+// in our case the github.io server
+
+// The user load the site olivabigyo.github.io/asyncdemo/frontend
+// The browser gets the index.html file from the github.io server,
+//      then the bootstrap.css file from the bootstrapcdn server,
+//      then the font.awesome.css file from the cdn server
+//      then the style.css file from the github.io server
+//      and the this code.js file too!
+// You can see this in the Network Tab by inspecting the site with right click
+// These are the first 5 GET requests
+
+// The browser runs this .js file on your handy, laptop, desktop etc.
+// This .js file will make requests to the backend server
+
+// Server Endpoint
+const apiEndpoint = 'http://amongus.olivabigyo.site/asyncdemo.php';
+// Local Endpoint while developing
 // const apiEndpoint = 'http://localhost/asyncdemo/backend/server.php';
-const apiEndpoint = 'https://amongus.olivabigyo.site/asyncdemo.php';
+
 
 // ------------------------------------------------------------------
 // Logging
+// -------
+
 
 const logBox = document.getElementById('logs');
 
 function log(entry, isError) {
-    if (isError) {
-        console.error(entry);
-    } else {
-        console.log(entry);
-    }
+
+    // Testing...
+    // if (isError) {
+    //     console.error(entry);
+    // } else {
+    //     console.log(entry);
+    // }
+
+    // create p for new entry
     const newEntry = document.createElement('p');
+    // populate with entry content
     newEntry.innerText = entry;
+    // display error entry with different style (red color)
     if (isError) {
         newEntry.classList.add('error');
     }
+    // place it
     logBox.appendChild(newEntry);
+    // We want to see the last logs, scroll down
     logBox.scrollTop = logBox.scrollHeight;
 }
 
@@ -27,13 +55,15 @@ function logError(entry) {
     log(entry, true)
 }
 
-// log('Downloading...');
+// Testing...
+// log('Something is happening...');
 // log('Done.');
 // log('Something went wrong', true);
 // logError('Something went wrong');
 
 // ------------------------------------------------------------------
 // Server requests
+// ---------------
 
 async function serverRequest(action, payload) {
     log(`Requesting ${action} from server...`);
@@ -60,10 +90,15 @@ async function serverRequest(action, payload) {
         log(`Successful ${action} request.`);
         return data;
     } catch (err) {
+        // Error handling
         console.log(err);
         logError('Exeption: ' + err);
     }
 }
+
+// ------------------------------------------------------------------
+// Update chat messages
+// --------------------
 
 async function getMessages() {
     const data = await serverRequest('getMessages');
@@ -79,13 +114,43 @@ function updateMessages(messages) {
         newMessage.innerText = message.name + ': ' + message.content;
         messageBox.appendChild(newMessage);
     }
+    // We have a fix-height box for the messages and
+    // we want to see the newest ones, which are at the bottom
     messageBox.scrollTop = messageBox.scrollHeight;
 }
 
 getMessages();
-setInterval(getMessages, 2000);
+// We want to update in every 5 seconds
+setInterval(getMessages, 5000);
+
 
 // ------------------------------------------------------------------
+// Submit the message
+// ----------------------
+
+async function addMessage(event) {
+    // async function, we don't want to reload the site
+    event.preventDefault();
+    // save name in localstorage
+    // yes, this is a tiny app without login etc.
+    saveName();
+    const name = document.getElementById('name');
+    const message = document.getElementById('message');
+    // we log everything to the logs panel to learn and understand more :)
+    log(`Sending new message from ${name.value}: ${message.value}`);
+    // this is our async request
+    const reply = await serverRequest('addMessage', {name: name.value, content: message.value});
+    if (reply) {
+        log('Message successfully sent.');
+        message.value = '';
+        // we repopulate the site and don't wait for the automatic repopulation which only happens in every 2 seconds
+        getMessages();
+    }
+}
+
+// ------------------------------------------------------------------
+// Save name in localStorage
+// -------------------------
 
 function saveName() {
     const name = document.getElementById('name');
@@ -102,22 +167,11 @@ function restoreName() {
 }
 // TODO: move to init()
 restoreName();
-
 // TODO: move to init()
 document.getElementById('message').focus();
 
-async function addMessage(event) {
-    event.preventDefault();
-    saveName();
-    const name = document.getElementById('name');
-    const message = document.getElementById('message');
-    log(`Sending new message from ${name.value}: ${message.value}`);
-    const reply = await serverRequest('addMessage', {name: name.value, content: message.value});
-    if (reply) {
-        log('Message successfully sent.');
-        message.value = '';
-        getMessages();
-    }
-}
+// ------------------------------------------------------------------
+// Event Listener
+// -------------------------
 
 document.getElementById('form').addEventListener('submit', addMessage);
